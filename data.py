29aff395107b8f77ce4e21cefd995398f7e53bc0
Mario <msiu001@fiu.edu>
2018-01-16 15:14:58 -0500

    This is 1st commit for a Library application in Python and Flask, with navbar routes ...

---
+++ data.py
@@ -0,0 +1,33 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article 1',
+            'body': 'This is the body content of article 1',
+            'author': 'Enzo Siu', 
+            'create_date': 'January 1st 2018'
+        },
+          {
+            'id': 2,
+            'title': 'Article 2',
+            'body': 'This is the body content of article 2',
+            'author': 'Mario Siu', 
+            'create_date': 'February 1st 2018'
+        },
+          {
+            'id': 3,
+            'title': 'Article 3',
+            'body': 'This is the body content of article 3',
+            'author': 'Yisel Garcia', 
+            'create_date': 'March 1st 2018'
+        },
+          {
+            'id': 4,
+            'title': 'Article 4',
+            'body': 'This is the body content of article 4',
+            'author': 'Lucky Siu', 
+            'create_date': 'April 1st 2018'
+        }
+    ]
+
+    return articles
